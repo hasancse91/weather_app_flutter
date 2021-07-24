bec7a0928b86d4d579d90de6508186ca9ecf53cb
Hasan Abdullah <hasan.cse91@gmail.com>
2021-07-24 22:29:02 +0600

    Code format

--- lib/network/api_interceptor.dart
+++ lib/network/api_interceptor.dart
@@ -3,14 +3,17 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:weather_app_flutter/config/build_config.dart';
 
 List<Interceptor> getInterceptors() {
-  return [_getPrettyLoggerInterceptor(), _getAppIdQueryParameterInterceptor()];
+  return [
+    _getPrettyLoggerInterceptor(),
+    _getAppIdQueryParameterInterceptor(),
+  ];
 }
 
 PrettyDioLogger _getPrettyLoggerInterceptor() {
   return PrettyDioLogger(requestHeader: true, responseHeader: true);
 }
 
-_getAppIdQueryParameterInterceptor() {
+Interceptor _getAppIdQueryParameterInterceptor() {
   var interceptor = InterceptorsWrapper(onRequest: (options, handler) {
     options.queryParameters.addAll(
       {'appid': BuildConfig.instance.config.appId},
